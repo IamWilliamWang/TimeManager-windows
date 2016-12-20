86e09d2b8e886cc76b69704cae03fb4ea4b8ea53
IamWilliamWang <iamjerichoholic@hotmail.com>
2016-12-20 10:48:34 +0800

    关机小程序

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace 关机小程序
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.关机ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.现在ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HalfMinuteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TenMinutesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.自定义ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重新启动ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.现在ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.HalfMinuteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TenMinutesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.自定义ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.取消指令ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxTime = new System.Windows.Forms.ComboBox();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.contextMenuStripButtonOk = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.注册关机事件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销毁关机事件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStripMainForm = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripComboBox透明度 = new System.Windows.Forms.ToolStripComboBox();
+            this.退出ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.显现ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开启动文件夹ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStripButtonOk.SuspendLayout();
+            this.contextMenuStripMainForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("幼圆", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关机ToolStripMenuItem,
+            this.重新启动ToolStripMenuItem,
+            this.取消指令ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 关机ToolStripMenuItem
+            // 
+            this.关机ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.现在ToolStripMenuItem,
+            this.HalfMinuteToolStripMenuItem,
+            this.TenMinutesToolStripMenuItem,
+            this.自定义ToolStripMenuItem});
+            this.关机ToolStripMenuItem.Name = "关机ToolStripMenuItem";
+            this.关机ToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.关机ToolStripMenuItem.Text = "关机";
+            // 
+            // 现在ToolStripMenuItem
+            // 
+            this.现在ToolStripMenuItem.Name = "现在ToolStripMenuItem";
+            this.现在ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.现在ToolStripMenuItem.Text = "现在";
+            this.现在ToolStripMenuItem.Click += new System.EventHandler(this.现在ToolStripMenuItem_Click);
+            // 
+            // HalfMinuteToolStripMenuItem
+            // 
+            this.HalfMinuteToolStripMenuItem.Name = "HalfMinuteToolStripMenuItem";
+            this.HalfMinuteToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.HalfMinuteToolStripMenuItem.Text = "半分钟";
+            this.HalfMinuteToolStripMenuItem.Click += new System.EventHandler(this.HalfMinuteToolStripMenuItem_Click);
+            // 
+            // TenMinutesToolStripMenuItem
+            // 
+            this.TenMinutesToolStripMenuItem.Name = "TenMinutesToolStripMenuItem";
+            this.TenMinutesToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.TenMinutesToolStripMenuItem.Text = "十分钟";
+            this.TenMinutesToolStripMenuItem.Click += new System.EventHandler(this.TenMinutesToolStripMenuItem_Click);
+            // 
+            // 自定义ToolStripMenuItem
+            // 
+            this.自定义ToolStripMenuItem.Name = "自定义ToolStripMenuItem";
+            this.自定义ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.自定义ToolStripMenuItem.Text = "自定义";
+            this.自定义ToolStripMenuItem.Click += new System.EventHandler(this.自定义ToolStripMenuItem_Click);
+            // 
+            // 重新启动ToolStripMenuItem
+            // 
+            this.重新启动ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.现在ToolStripMenuItem1,
+            this.HalfMinuteToolStripMenuItem1,
+            this.TenMinutesToolStripMenuItem1,
+            this.自定义ToolStripMenuItem1});
+            this.重新启动ToolStripMenuItem.Name = "重新启动ToolStripMenuItem";
+            this.重新启动ToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.重新启动ToolStripMenuItem.Text = "重新启动";
+            // 
+            // 现在ToolStripMenuItem1
+            // 
+            this.现在ToolStripMenuItem1.Name = "现在ToolStripMenuItem1";
+            this.现在ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.现在ToolStripMenuItem1.Text = "现在";
+            this.现在ToolStripMenuItem1.Click += new System.EventHandler(this.现在ToolStripMenuItem1_Click);
+            // 
+            // HalfMinuteToolStripMenuItem1
+            // 
+            this.HalfMinuteToolStripMenuItem1.Name = "HalfMinuteToolStripMenuItem1";
+            this.HalfMinuteToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.HalfMinuteToolStripMenuItem1.Text = "半分钟";
+            this.HalfMinuteToolStripMenuItem1.Click += new System.EventHandler(this.HalfMinuteToolStripMenuItem1_Click);
+            // 
+            // TenMinutesToolStripMenuItem1
+            // 
+            this.TenMinutesToolStripMenuItem1.Name = "TenMinutesToolStripMenuItem1";
+            this.TenMinutesToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.TenMinutesToolStripMenuItem1.Text = "十分钟";
+            this.TenMinutesToolStripMenuItem1.Click += new System.EventHandler(this.TenMinutesToolStripMenuItem1_Click);
+            // 
+            // 自定义ToolStripMenuItem1
+            // 
+            this.自定义ToolStripMenuItem1.Name = "自定义ToolStripMenuItem1";
+            this.自定义ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.自定义ToolStripMenuItem1.Text = "自定义";
+            this.自定义ToolStripMenuItem1.Click += new System.EventHandler(this.自定义ToolStripMenuItem1_Click);
+            // 
+            // 取消指令ToolStripMenuItem
+            // 
+            this.取消指令ToolStripMenuItem.Name = "取消指令ToolStripMenuItem";
+            this.取消指令ToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.取消指令ToolStripMenuItem.Text = "取消指令";
+            this.取消指令ToolStripMenuItem.Click += new System.EventHandler(this.取消指令ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // comboBoxTime
+            // 
+            this.comboBoxTime.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBoxTime.FormattingEnabled = true;
+            this.comboBoxTime.Location = new System.Drawing.Point(90, 62);
+            this.comboBoxTime.Name = "comboBoxTime";
+            this.comboBoxTime.Size = new System.Drawing.Size(109, 22);
+            this.comboBoxTime.TabIndex = 1;
+            this.comboBoxTime.Text = "0";
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMode.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "关机",
+            "重启"});
+            this.comboBoxMode.Location = new System.Drawing.Point(13, 62);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(71, 22);
+            this.comboBoxMode.TabIndex = 2;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.ContextMenuStrip = this.contextMenuStripButtonOk;
+            this.buttonOK.Location = new System.Drawing.Point(205, 62);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 3;
+            this.buttonOK.Text = "确定";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // contextMenuStripButtonOk
+            // 
+            this.contextMenuStripButtonOk.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.注册关机事件ToolStripMenuItem,
+            this.销毁关机事件ToolStripMenuItem,
+            this.打开启动文件夹ToolStripMenuItem});
+            this.contextMenuStripButtonOk.Name = "contextMenuStrip1";
+            this.contextMenuStripButtonOk.Size = new System.Drawing.Size(161, 92);
+            // 
+            // 注册关机事件ToolStripMenuItem
+            // 
+            this.注册关机事件ToolStripMenuItem.Name = "注册关机事件ToolStripMenuItem";
+            this.注册关机事件ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.注册关机事件ToolStripMenuItem.Text = "注册关机事件";
+            this.注册关机事件ToolStripMenuItem.Click += new System.EventHandler(this.注册关机事件ToolStripMenuItem_Click);
+            // 
+            // 销毁关机事件ToolStripMenuItem
+            // 
+            this.销毁关机事件ToolStripMenuItem.Name = "销毁关机事件ToolStripMenuItem";
+            this.销毁关机事件ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.销毁关机事件ToolStripMenuItem.Text = "销毁关机事件";
+            this.销毁关机事件ToolStripMenuItem.Click += new System.EventHandler(this.销毁关机事件ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "选择：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "时间（分钟）：";
+            // 
+            // contextMenuStripMainForm
+            // 
+            this.contextMenuStripMainForm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox透明度,
+            this.显现ToolStripMenuItem,
+            this.退出ToolStripMenuItem1});
+            this.contextMenuStripMainForm.Name = "contextMenuStripMainForm";
+            this.contextMenuStripMainForm.Size = new System.Drawing.Size(182, 77);
+            // 
+            // toolStripComboBox透明度
+            // 
+            this.toolStripComboBox透明度.Items.AddRange(new object[] {
+            "0",
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "55",
+            "60",
+            "65",
+            "70",
+            "75",
+            "80",
+            "85",
+            "90",
+            "95",
+            "100"});
+            this.toolStripComboBox透明度.Name = "toolStripComboBox透明度";
+            this.toolStripComboBox透明度.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox透明度.Text = "透明度";
+            this.toolStripComboBox透明度.TextChanged += new System.EventHandler(this.toolStripComboBox透明度_TextChanged);
+            // 
+            // 退出ToolStripMenuItem1
+            // 
+            this.退出ToolStripMenuItem1.Name = "退出ToolStripMenuItem1";
+            this.退出ToolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
+            this.退出ToolStripMenuItem1.Text = "退出";
+            this.退出ToolStripMenuItem1.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 显现ToolStripMenuItem
+            // 
+            this.显现ToolStripMenuItem.Name = "显现ToolStripMenuItem";
+            this.显现ToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.显现ToolStripMenuItem.Text = "任务栏显现";
+            this.显现ToolStripMenuItem.Click += new System.EventHandler(this.显现ToolStripMenuItem_Click);
+            // 
+            // 打开启动文件夹ToolStripMenuItem
+            // 
+            this.打开启动文件夹ToolStripMenuItem.Name = "打开启动文件夹ToolStripMenuItem";
+            this.打开启动文件夹ToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.打开启动文件夹ToolStripMenuItem.Text = "打开启动文件夹";
+            this.打开启动文件夹ToolStripMenuItem.Click += new System.EventHandler(this.打开启动文件夹ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 143);
+            this.ContextMenuStrip = this.contextMenuStripMainForm;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.comboBoxTime);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.96D;
+            this.ShowInTaskbar = false;
+            this.Text = "关机管理";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStripButtonOk.ResumeLayout(false);
+            this.contextMenuStripMainForm.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 关机ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 现在ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HalfMinuteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TenMinutesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 自定义ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重新启动ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 现在ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem HalfMinuteToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem TenMinutesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 自定义ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 取消指令ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBoxTime;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripButtonOk;
+        private System.Windows.Forms.ToolStripMenuItem 注册关机事件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 销毁关机事件ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMainForm;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox透明度;
+        private System.Windows.Forms.ToolStripMenuItem 显现ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开启动文件夹ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 关机小程序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i=0;i<=60;i+=5)
+                this.comboBoxTime.Items.Add(i);
+            comboBoxMode.SelectedIndex = 0;
+            
+        }
+
+        static String system(String command)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = "cmd";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.WriteLine("exit");
+
+            return process.StandardOutput.ReadToEnd();
+        }
+
+        private void runShutdownCommand(Mode mode)
+        {
+            if (mode == Mode.取消)
+                runCommand(Mode.取消, -1);
+        }
+
+        private void runCommand(Mode mode, float seconds)
+        {
+            runCommand(mode, (int)seconds);
+        }
+
+        private void runCommand(Mode mode, int seconds)
+        {
+            String command = "shutdown ";
+            switch (mode)
+            {
+                case Mode.关机:
+                    command += "-s -t " + seconds;
+                    break;
+                case Mode.重启:
+                    command += "-g -t " + seconds;
+                    break;
+                case Mode.取消:
+                    command += "-a";
+                    break;
+                default:
+                    MessageBox.Show("Error!");
+                    return;
+            }
+            system(command);
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            switch (this.comboBoxMode.Text)
+            {
+                case "关机":
+                    runCommand(Mode.关机, float.Parse(this.comboBoxTime.Text) * 60);
+                    break;
+                case "重启":
+                    runCommand(Mode.重启, float.Parse(this.comboBoxTime.Text) * 60);
+                    break;
+            }
+
+        }
+       
+        private void 现在ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("现在要关机吗？", "警告", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+                return;
+            runCommand(Mode.关机, 0);
+            return;
+        }
+
+        private void HalfMinuteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            runCommand(Mode.关机, 30);
+            return;
+        }
+
+        private void TenMinutesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            runCommand(Mode.关机, 600);
+            return;
+        }
+
+        private void 自定义ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float second = float.Parse(Interaction.InputBox("几分钟呢？", "关机时间选择", "", -1, -1)) * 60;
+                runCommand(Mode.关机, second);
+            }
+            catch
+            {
+                MessageBox.Show("输入错误！");
+            }
+        }
+
+        private void 现在ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("现在要重启电脑吗？", "警告", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+                return;
+            runCommand(Mode.重启, 0);
+            return;
+        }
+
+        private void HalfMinuteToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            runCommand(Mode.重启, 30);
+            return;
+        }
+
+        private void TenMinutesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            runCommand(Mode.重启, 600);
+            return;
+        }
+
+        private void 自定义ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try {
+                float second = float.Parse(Interaction.InputBox("几分钟：", "重启时间选择", "", -1, -1)) * 60;
+                runCommand(Mode.重启, second);
+            }
+            catch
+            {
+                MessageBox.Show("输入错误！");
+            }
+        }
+
+        private void 取消指令ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            runShutdownCommand(Mode.取消);
+            return;
+        }
+
+        private void 注册关机事件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            float minutes = float.Parse(this.comboBoxTime.Text);
+            if (minutes == 0)
+            {
+                MessageBox.Show("选择0分钟是危险行为！已阻止");
+                return;
+            }
+            try {
+                File.WriteAllText(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\autoshutdown.cmd", "shutdown -s -t " + minutes * 60);
+            }
+            catch {
+                MessageBox.Show("失败！请使用管理员权限重启本程序");
+                return;
+            }
+            MessageBox.Show("注册成功！");
+        }
+
+        private void 销毁关机事件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\autoshutdown.cmd", "");
+            }
+            catch
+            {
+                MessageBox.Show("失败！请使用管理员权限重启本程序");
+                return;
+            }
+            system("del " + "\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\autoshutdown.cmd\"");
+            MessageBox.Show("销毁成功！");
+        }
+
+        private void toolStripComboBox透明度_TextChanged(object sender, EventArgs e)
+        {
+            this.Opacity = float.Parse(this.toolStripComboBox透明度.Text) / 60;
+        }
+
+        private void 显现ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+        }
+
+        private void 打开启动文件夹ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Explorer.exe", @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\");
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        enum Mode { 取消, 关机, 重启 };
+    }
+}
